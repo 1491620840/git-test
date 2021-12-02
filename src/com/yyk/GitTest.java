3422474a40f7698e53b1195fb976a965d9320e0e
Yyk <Yyk@qq.com>
2021-12-02 17:26:57 +0800

    first commit

---
+++ src/com/yyk/GitTest.java
@@ -0,0 +1,9 @@
+package com.yyk;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git1");
+        System.out.println("hello git2");
+    }
+}
